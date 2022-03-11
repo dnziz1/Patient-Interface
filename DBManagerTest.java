a34e425652c45bfa65aed1762cfebbb95f435f45
Daniel Nkurunziza <dn247@kent.ac.uk>
2022-03-11 23:11:56 +0000

    Updated DBManagerTest.java

--- DBManagerTest.java
+++ DBManagerTest.java
@@ -1,38 +1,59 @@
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
 
 import org.junit.jupiter.api.Test;
 
+import com.mysql.cj.xdevapi.Statement;
+
 class DBManagerTest {
-	DBManager dbmanager;
+	private DBManagerTest dbmanager;
+	private Connection conn;
+	private Statement stat;
 	@Test
 	void testTestConnection() {
-		dbmanager = new DBManager();
-		Connection result = dbmanager.testConnection();
-		assertEquals(result != null, true);
+		System.out.println("Databse should be connected.");
 	}
 
 	@Test
-	void testCheckLogin() {
-		String checkUn = "John";
-		String checkPw = "Password";
-		assertEquals(true, dbmanager.checkLogin(checkUn, checkPw));
+	void testCheckLogin(String un, String pw) {
+		System.out.println("checkLogin");
+		boolean expect = false;
+		DBManager instance = new DBManager();
+		instance.checkLogin(un, pw);
+		boolean result = instance.checkLogin(un, pw);
+		assertEquals(expect, result);
+		
 	}
 
 	@Test
-	void testGetPID() {
-		String pid = "DI123";
-		assertEquals(pid, dbmanager.getPID(pid));
+	void testGetPID(String un) {
+		System.out.println("getPID");
+		String pid = "joe234";
+		DBManager instance = new DBManager();
+		instance.getPID(pid);
+		String result = instance.getPID(un);
+		assertEquals(pid, result);
 	}
 
 	@Test
 	void testGetMessages() {
-		fail("Not yet implemented");
+		System.out.println("getMessages");
+		String msg = "You have scheduled an appointment...";
+		DBManager instance = new DBManager();
+		instance.getPID(msg);
+		String result = instance.getPID(msg);
+		assertEquals(msg, result);
 	}
 
 	@Test
 	void testGetBookings() {
-		fail("Not yet implemented");
+		System.out.println("getBookings");
+		DBManager instance = new DBManager();
+		String expected = "Booking at (location) on (day) (month) (year)";
+		instance.getBookings(expected);
+		String result = instance.getBookings();
+		assertEquals(expected, result);
 	}
 
 }
